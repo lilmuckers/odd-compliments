6d328db6661b1119ca003a9744ba39898d387e14
Patrick McKinley <patrick@patrick-mckinley.com>
2013-06-16 18:44:45 +0100

    Added color animations

--- compliments.js
+++ compliments.js
@@ -1,8 +1,29 @@
 var Compliments = function(filename){
   jQuery.get(filename, this.parseCompliments.bind(this))
+  this.container = jQuery('#compliment');
+  this.body = jQuery('body');
+  this.delay = 5000;
 }
 Compliments.prototype.parseCompliments = function(data){
-  console.log(data);
+  this.colors = data.colors;
+  this.compliments = data.compliments;
+  this.rotateCompliment();
+}
+Compliments.prototype.rotateCompliment = function(){
+  //fade out old text
+  this.container.fadeOut(200, function(){
+      //obtain the new colour and compliment
+      var color = this.colors[Math.floor(Math.random()*this.colors.length)];
+      var compliment = this.compliments[Math.floor(Math.random()*this.compliments.length)];
+      
+      //update the container with the text
+      this.container.text(compliment);
+      
+      //change the background color
+      this.body.
+    }.bind(this)
+  );
+  
 }
 
 
