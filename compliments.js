15257f37b662a5fc655cdded7ac8d9f451c5358b
Patrick McKinley <patrick@patrick-mckinley.com>
2013-06-16 23:17:52 +0100

    Added some event based navigation

--- compliments.js
+++ compliments.js
@@ -1,17 +1,65 @@
 var Compliments = function(filename){
-  jQuery.get(filename, this.parseCompliments.bind(this))
   this.container = jQuery('#compliment');
   this.body = jQuery('body');
+  this.list = jQuery('#complimentList');
   this.delay = 10000;
+  jQuery.get(filename, this.parseCompliments.bind(this));
+  jQuery(window).keyup(this.keyChange.bind(this));
+}
+Compliments.prototype.keyChange = function(event){
+  switch(event.keyCode){
+    //prev
+    case 38:
+    case 37:
+      var index = this.currentIndex - 1;
+      if(index < 0){
+        index = this.compliments.length-1;
+      }
+      break;
+    //next
+    case 39:
+    case 40:
+      var index = this.currentIndex + 1;
+      if(index >= this.compliments.length){
+        index = 0;
+      }
+      break;
+  }
+  if(index){
+    if(this.timeoutThing){
+      clearTimeout(this.timeoutThing);
+    }
+    this.switchCompliment(index);
+  }
 }
 Compliments.prototype.parseCompliments = function(data){
   this.colors = data.colors;
   this.compliments = data.compliments;
+  
+  //create the list
+  for(var i=0;i < this.compliments.length; i++){
+    var item = jQuery('<div></div>');
+    item.data('index', i);
+    item.click(this.switchClick.bind(this));
+    this.list.append(item);
+  }
+  
+  //hashhashhash
   if(location.hash){
     var complimentIndex = location.hash.substring(1);
     this.switchCompliment(parseInt(complimentIndex));
   } else {
     this.rotateCompliment();
+  }
+}
+Compliments.prototype.switchClick = function(event){
+  var index = jQuery(event.target).data('index');
+  
+  if(index){
+    if(this.timeoutThing){
+      clearTimeout(this.timeoutThing);
+    }
+    this.switchCompliment(index);
   }
 }
 Compliments.prototype.rotateCompliment = function(){
@@ -26,9 +74,12 @@ Compliments.prototype.switchCompliment = function(index){
   if(!index){
     var index = Math.floor(Math.random()*this.compliments.length);
     //History.pushState({state:1}, "Odd Compliment #"+History.getHash(), '#'+History.getHash());
-    window.location.hash = index;
-    setTimeout(this.rotateCompliment.bind(this), this.delay);
+    this.timeoutThing = setTimeout(this.rotateCompliment.bind(this), this.delay);
   }
+  
+  //set the hash
+  window.location.hash = index;
+  this.currentIndex = index;
   
   //get the compliment
   var compliment = this.compliments[index];
