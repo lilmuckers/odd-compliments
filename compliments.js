5974029f62d21181afbe2aa23d3411ac3ebfaf6f
Patrick McKinley <patrick@patrick-mckinley.com>
2013-06-16 18:28:29 +0100

    Changed jquery ajax stuff

--- compliments.js
+++ compliments.js
@@ -1,5 +1,5 @@
 var Compliments = function(filename){
-  jQuery.ajax(filename, this.parseCompliments.bind(this))
+  jQuery.get(filename, this.parseCompliments.bind(this))
 }
 Compliments.prototype.parseCompliments = function(data){
   console.log(data);
